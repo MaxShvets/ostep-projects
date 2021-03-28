b642c27208b09f7a7fd6980bc9fdbdb670e73aba
MaxShvetsRing <maksym.shvets@ring.com>
2021-03-28 12:50:50 +0300

    Cleare allocated memory

--- processes-shell/wish.c
+++ processes-shell/wish.c
@@ -5,6 +5,8 @@
 #include <sys/errno.h>
 #include <unistd.h>
 
+#define DEBUG
+
 typedef struct str_list_node {
   char *str;
   struct str_list_node *next;
@@ -16,8 +18,11 @@ typedef struct str_list {
   StringListNode *end;
 } StringList;
 
-StringList str_list_init() {
-  StringList list = { 0, NULL, NULL  };
+StringList* str_list_init() {
+  StringList *list = malloc(sizeof(StringList));
+  list->len = 0;
+  list->start = NULL;
+  list->end = NULL;
   return list;
 }
 
@@ -36,19 +41,28 @@ int str_list_append_item(StringList *list, char *str) {
   return ++(list->len);
 }
 
-void str_list_output(StringList *list) {
-  StringListNode *node;
-  int i;
-    
-  for (node = list->start, i = 0; node != NULL; i++, node = node->next) {
-    printf("%d: %s", i, node->str);
-  } 
+void str_list_free(StringList *list) {
+  StringListNode *node = list->start;
+  
+  while (node != NULL) {
+    StringListNode *next_node = node->next;
+    free(node->str);
+    free(node);
+    node = next_node;
+  }
+
+  free(list);
 }
 
 typedef struct command {
   char *name;
-  StringList args;
+  StringList *args;
 } Command;
+
+void clear_command(Command command) {
+  free(command.name);
+  str_list_free(command.args);
+}
 
 int consume_whitespace() {
   char c;
@@ -106,12 +120,12 @@ Command get_command() {
   char *name = NULL;
   get_word(&name);
 
-  StringList args_list = str_list_init();
+  StringList *args_list = str_list_init();
 
   while (consume_whitespace() != 1) {
     char *word = NULL;
     get_word(&word);
-    str_list_append_item(&args_list, word);
+    str_list_append_item(args_list, word);
   }
 
   Command command = { name, args_list };
@@ -133,12 +147,12 @@ char *get_command_path(Command command) {
 }
 
 char **get_command_args_array(Command command) {
-  char **array = malloc(command.args.len * sizeof(char *) + 2);
+  char **array = malloc(command.args->len * sizeof(char *) + 2);
   array[0] = command.name;
   StringListNode *node;
   int i;
   
-  for (i = 1, node = command.args.start; node != NULL; i++, node = node->next) {
+  for (i = 1, node = command.args->start; node != NULL; i++, node = node->next) {
     array[i] = node->str;
   }
 
@@ -153,8 +167,14 @@ void execute_system_command(Command command) {
     fprintf(stderr, "failed to create a subprocess: %s", strerror(errno));
   } else if (rc == 0) {
     char *command_path = get_command_path(command);
-    printf("command path: %s\n", command_path);
     char **args_array = get_command_args_array(command);
+    
+#ifdef DEBUG
+    printf("Command path: %s, %p\n", command_path, command_path);
+    for (int i = 0; args_array[i] != NULL; i++) {
+      printf("arg: %s, %p\n", args_array[i], args_array + i);
+    }
+#endif
     
     execv(command_path, args_array);
     fprintf(stderr, "something went wrong\n");
@@ -177,5 +197,6 @@ int main(int argc, char *argv[]) {
   while (1) {
     command = get_command();
     execute_command(command);
+    clear_command(command);
   }
 }
